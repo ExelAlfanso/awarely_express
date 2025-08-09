e7e88295855dbaf1f6553deef318c7eaa1a3a4ce
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-08-09 14:59:16 +0700

    import: mongoose

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-const db = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: false,
-});
-db.on("connect", () => {
-  console.log("Connected to the database");
-});
+// const { Pool } = require("pg");
+// require("dotenv").config();
+// const db = new Pool({
+//   connectionString: process.env.DB_URL,
+//   ssl: false,
+// });
+// db.on("connect", () => {
+//   console.log("Connected to the database");
+// });
 
-module.exports = db;
+// module.exports = db;
+
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("✅ Connected to MongoDB");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+export default connectToDB;
